2d2075c179f1d9a9f87e3201ba51628e570956d2
jarlouval <jarlou.valenzuela@j2-b.com>
2022-12-13 16:03:39 +0800

    updated to sign-in for welcome page

--- ReferAllProject/src/features/home/route.js
+++ ReferAllProject/src/features/home/route.js
@@ -1,6 +1,7 @@
+import { SignIn } from '../authentication';
 import { WelcomePage } from './';
 
 export default {
   path: '',
-  childRoutes: [{ path: 'welcome-page', component: WelcomePage, isIndex: true }],
+  childRoutes: [{ path: 'welcome-page', component: SignIn, isIndex: true }],
 };
